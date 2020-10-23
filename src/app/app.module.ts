ee359634cfe0ba62bc8fff65a3ee230fcd97fc98
Nicolau Sunyer <nsunyer@gmail.com>
2020-10-23 17:30:48 +0200

    Arreglats bugs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CanvasModule } from '@ng-web-apis/canvas/';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CanvasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
